fe1960f69ccbd196d44be1d5aea2f3a33e4e8b42
Baptiste Giannesini <b.giannesini@gmail.com>
2021-01-22 05:12:40 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+<meta charset="UTF-8">
+<script src="https://kit.fontawesome.com/eb85e4a2ad.js" crossorigin="anonymous"></script>
+<link href="styles/default.css" rel="stylesheet">
+
+<title></title>
+</head>
+<body>
+<div class="big-container flex-col">
+  
+<!-- Barre navidation -->
+    <header  class="navbar flex-line">
+        <div class="navbar-top flex-line ">
+            <div class="navbar-item">
+                <a href="#" alt="top" title="back to top">
+                    <i class="fas fa-home"></i> 
+                </a>
+            </div>
+            <div class="navbar-item">
+                <a href="#" alt="portfolio" title="Portfolio">
+                    <i class="fas fa-briefcase"></i>
+                </a>
+            </div>
+            <div class="navbar-item">
+                <a href="#" Alt="cv-download" title="download CV">
+                    <i class="fas fa-file-download"></i>
+                </a>
+            </div>
+        </div>
+        <h1>dev.tegristh.fr</h1>
+         <div>
+             <p id="gnomebar"></p>
+        </div>
+        
+        <div class="navbar-bottom flex-line">
+            <div class="navbar-item">
+                <a href="https://www.linkedin.com/in/baptiste-giannesini-aab022178/" alt="LinkedIn"   target="_blank" title="LinkedIn">
+                    <i class="fab fa-linkedin"></i>
+                </a>
+            </div>
+            <div class="navbar-item">
+                <a href="https://github.com/Tegristh" alt="GitHub"  target="_blank" title="GitHub">
+                    <i class="fab fa-github-square"></i>
+                </a>
+            </div>
+            <div class="navbar-item">
+                <a href="https://codepen.io/tegristh" target="_blank" alt="CodePen" title="CodePen">
+                    <i class="fab fa-codepen"></i>
+                </a>
+            </div>
+        </div>
+</header>
+<!-- Hero banner style -->
+    <div class="presentation">
+        
+        <figure class="presentation-image flex-line">
+            <img src="https://avatars.githubusercontent.com/u/63445829?s=400&u=1e0c41a667caab52f3fd497ec0246906acfd475f&v=4" alt="ma-truffe"  >
+        
+        <figcaption class="presentation-texte">
+        <p class="salute">Bonjour! je m'appelle <em>Baptiste</em>, j'apprend le developpement Web!</p>
+        <p>Ancien conducteur routier de nuit, je me suis reconverti en tant que technicien d'usinage sur commandes numériques il y'a quelques mois.</p>
+        <p>Dans ma région, la majorité de l'activité en usinage provient de la filiaire aéronautique touchée de plein fouet par la crise économique et sanitaire actuelle.</p>
+        <p>Plutôt que de m'épuiser en recherches infructueuses ou rester inactif, j'ai essayé de tirer le meilleur parti du marasme ambiant pour reprendre mon apprentissage du code.</p>
+        <p>Je suis actuellement en recherche d'un contrat de professionalisation pour obtenir un diplôme de Développeur Web reconnu au RNCP.</p>
+</figcaption></figure>
+    </div>
+<!-- Portfolio -->
+<h2>Mes réalisations à ce jour:</h2>    
+    <div class="Portfolio flex-line">
+        <div class="integration flex-col">
+            <h3>Intégration </h3>
+            <div class="projet"></div>
+            <div class="projet"></div>
+        </div>
+        <div class="php flex-col">
+            <h3>PHP</h3>
+            <div class="projet"></div>
+            <div class="projet"></div>
+        </div>
+        <div class="javascript flex-col">
+            <h3>JavaScript</h3>
+            <div class="projet"></div>
+            <div class="projet"></div>
+        </div>
+        <div class="cms flex-col">
+            <h3>Wordpress et autres CMS</h3>
+            <div class="projet"></div>
+        </div>
+    </div>
+<!-- Footer -->
+    <footer>
+    </footer>
+</div>
+<script type="text/javascript" src="scripts/heure.js"></script>
+</body>
+</html>
---
+++ scripts/heure.js
@@ -0,0 +1,13 @@
+var boite = document.querySelector('#gnomebar');
+function horloge()
+{
+var d = new Date();
+var hours = d.getHours()+"H "+d.getMinutes()+'M '+d.getSeconds()+'s ';
+var tabMonth = new Array("Janvier", "Fevrier", "Mars", "Avril", "Mai", "Juin", "Juillet", "Aout", "Septembre", "Octobre", "Novembre", "Decembre");
+var date = d.getDate()+' '+(tabMonth[d.getMonth()]) +' '+d.getFullYear();
+  
+var fullDate = hours +` |  Le `+ date;
+boite.textContent = fullDate;
+console.log(fullDate);
+}
+setInterval("horloge()",1000)
